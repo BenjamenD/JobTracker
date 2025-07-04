7b722fdda2bd647503fbf868463216f7d8b6a44a
Ben <bendelucry@gmail.com>
2025-07-04 13:32:15 -0300

    added mongodb connection

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,20 @@
 import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import dotenv from 'dotenv';
+import { connectDB } from './config/db.js'
+
 
 const app = express();
+dotenv.config()
+
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
-    res.send("server is ready")
+    
 });
 
 app.listen(PORT, () => {
+    connectDB();
     console.log(`server started at http://localhost:${PORT}`);
 });
