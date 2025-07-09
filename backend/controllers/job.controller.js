95a3e80b0a814f63c511f0af94e84550855a7327
Ben <bendelucry@gmail.com>
2025-07-09 13:22:51 -0300

    added job controller

---
+++ backend/controllers/job.controller.js
@@ -0,0 +1,36 @@
+import Job from "../models/job.model";
+
+export const getJobs = async (req, res) => {
+    try {
+        const jobs = await Job.find();
+
+        if(!jobs){
+            console.log("Error getting jobs");
+            res.status(404).json({msg: "Error getting jobs"});
+        }
+        
+        res.status(200).json(jobs)
+
+    } catch (error) {
+        console.error(`Error getting jobs: ${error.message}`);
+        res.status(500).json({msg: "Server error while getting jobs"});
+    }
+}
+
+export const getJob = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const job = await Job.findById(id)
+
+        if(!job){
+            console.log("Could not find requested job")
+            res.status(404).json({msg: "Could not find requested job"})
+        }
+
+        res.status(200).json(job)
+
+    } catch (error) {
+        console.error(`Error finding requested job: ${error.message}`);
+        res.status(500).json({msg: "Error finding requested job"})
+    }
+}
