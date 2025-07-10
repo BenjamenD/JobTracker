9a13130d7151fbb4f73d37f6bafe00f7c1b916c5
Ben <bendelucry@gmail.com>
2025-07-10 12:12:18 -0300

    added pages and router for frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,23 @@
-function App() {
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider, Routes } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import MainLayout from './layouts/MainLayout'
+import LoginPage from './pages/LoginPage'
+import JobPage from './pages/JobPage'
+import UserPage from './pages/UserPage'
 
-  return (
-    <>
-      <p>hello there</p>
-    </>
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<MainLayout />}>
+      <Route index element={<HomePage />}/>
+      <Route path="/login" element={<LoginPage />}/>
+      <Route path="/user" element={<UserPage />}/>
+      <Route path="/job" element={<JobPage />}/>
+    </Route>
   )
-};
+)
+
+function App() {
+  return (<RouterProvider router={router}/>)
+}
 
 export default App
