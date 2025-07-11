37f976941b404dd794315a2cbcd82704f40504b6
Ben <bendelucry@gmail.com>
2025-07-11 20:55:12 -0300

    added deployment configurations to server.js

--- backend/server.js
+++ backend/server.js
@@ -26,6 +26,14 @@ app.use("/api/bookmark", bookmarkRoutes);
 
 console.log(`NODE_ENV: ${process.env.NODE_ENV}`)
 
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+    
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+    })
+}
+
 app.listen(PORT, () => {
     connectDB();
     if(process.env.NODE_ENV === "development"){
